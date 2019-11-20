eea95d7cceb0834be8c892e0e06604c37edf5b7e
Jerrett Rende <rende99@gmail.com>
2019-11-20 01:48:34 -0500

    various flow changes

--- signup.js
+++ signup.js
@@ -2,12 +2,21 @@
 
 export default function pageLoaded(){
     $('#submitButton').on("click", async function(){
+        event.preventDefault();
+        //Reveal more
+        $('#hid1').css("visibility", "visible");
+        $('#hid2').css("visibility", "visible");
+
+
+    });
+    $('#finishButton').on("click", async function(){
         //create new
+        event.preventDefault();
         let userName = $('#userEmail').val();
         let pass = $('#userPassword').val();
         //need to check if account exists. If it does, send to login page. If not, create a new Account!
 
-        const response = await axios({
+        const createResponse = await axios({
             method: "post",
             url: "http://localhost:3000/account/create",
             data: {
@@ -15,9 +24,53 @@ export default function pageLoaded(){
                 pass: pass
             },
         });
+        let userGender = $('#genderSelector input:radio:checked').val()
+        let userAge = $('#userAge').val();
+        let userLocation = $('#stateSelector').val();
+        let userEducation = $('#educationSelector').val();
+        let userRace = $('#raceSelector').val();
+        let userAffiliation = $('#affiliationSelector input:radio:checked').val();
+        
+        //need to get 'Bearer' auth jws token to send as part of this request.
+        const loginResponse = await axios({
+            method: "post",
+            url: "http://localhost:3000/account/login",
+            data: {
+                name: userName,
+                pass: pass
+            },
+        });
+        let token = loginResponse.data.jwt;
+        
+        const response = await axios({
+            method: "post",
+            url: "http://localhost:3000/user/demo",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+            data: {
+                userGender: userGender,
+                userAge: userAge,
+                userLocation: userLocation,
+                userEducation: userEducation,
+                userRace: userRace,
+                userAffiliation: userAffiliation
+            },
+        });
 
-        window.location.href = "tutorial.html";
+
+        /*
+        await populateDemographics({
+            userGender: userGender,
+            userAge: userAge,
+            userLocation: userLocation,
+            userEducation: userEducation,
+            userRace: userRace,
+            userAffiliation: userAffiliation
+        });
+        */
     });
+
 }
 
 
@@ -27,5 +80,4 @@ export default function pageLoaded(){
 
 $(document).ready(function () {
     pageLoaded();
-
 });
